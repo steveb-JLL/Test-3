be95d7a340a33b2ae40cdb5189026f81e46e6047
Butcher <Stephen.Butcher@eu.jll.com>
2017-12-22 02:11:59 +0000

    Add project files.

---
+++ Test-3/Models/Album.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test_3.Models
+{
+    public class Album
+    {
+        public int AlbumID { get; set; }
+
+        public string Title { get; set; }
+        
+        public List<Review> Reviews { get; set; }
+
+    }
+}
---
+++ Test-3/Models/Review.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Test_3.Models
+{
+    public class Review
+    {
+        public int ReviewID { get; set; }
+
+        public int AlbumID { get; set; }
+
+        public virtual Album Album { get; set; }
+
+        public string Content { get; set; }
+
+        [Required]
+        [Display(Name ="Email Address")]
+        [DataType(DataType.EmailAddress)]
+        public string ReviewerEmail { get; set; }
+
+    }
+}
